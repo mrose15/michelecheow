0fe60979102f22e9fdab26054b08dec69bdc617a
Michele Rose <ldnallen@gmail.com>
2016-09-13 11:33:41 -0700

    core func

---
+++ wp-content/plugins/michelecheow-core-func/core-functionality.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Name: Core Functionality (michelecheow.com)
+ * Plugin URI: https://michelecheow.com/
+ * Functionality
+ * Description: This contains all your site's core functionality so that it is theme independent. <strong>It should always be activated</strong>.
+ * Version:     1.1.0
+ * Author:      michelecheow.com
+ * Author URI:  http://michelecheow.com
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2, as published by the
+ * Free Software Foundation.  You may NOT assume that you can use any other
+ * version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ *
+ * @author     Michele Cheow
+ * @version    1.1.0
+ * @package    CoreFunctionality
+ * @copyright  Copyright (c) 2014, Michele Cheow, Bill Erickson & Jared Atchison
+ * @link       https://github.com/billerickson/Core-Functionality
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+// Plugin Directory 
+define( 'EA_DIR', dirname( __FILE__ ) );
+ 
+// General
+include_once( EA_DIR . '/inc/general.php' );
+
