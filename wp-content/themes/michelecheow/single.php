1d9f22e7d7310bbaa3653d2601845a76ef6ddc04
Michele Rose <ldnallen@gmail.com>
2016-09-14 16:26:46 -0700

    Remove header right on single post

--- wp-content/themes/michelecheow/single.php
+++ wp-content/themes/michelecheow/single.php
@@ -10,6 +10,9 @@
 /** Remove default sidebar */
 remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
 
+//* Remove the header right widget area
+unregister_sidebar( 'header-right' );
+
 /* Code to Display Featured Image on top of the post */
 add_action( 'genesis_before_entry', 'featured_post_image', 8 );
 function featured_post_image() {
